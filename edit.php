7de7ab3873c0e42bc33231b33f97349ee7476f6e
bobo <linbow@foxmail.com>
2017-05-03 15:53:28 +0800

    添加了管理员登陆功能

--- edit.php
+++ edit.php
@@ -1,17 +1,3 @@
-<!doctype html>
-<html lang="zh-CN">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.css">
-    <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <title>website</title>
-    <link href="css/new.css" type="text/css" rel="stylesheet">
-</head>
-<body>
 <?php include "includes/nav.html";
 include "includes/cnn.php";
 ?>
@@ -48,6 +34,4 @@ include "includes/cnn.php";
         <?php include"includes/right.php"?>
     </div>
 </div>
-</body>
 <?php include "includes/footer.html"; ?>
-</html>
---
+++ login.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.css">
+    <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <title>try</title>
+    <link href="css/new.css" type="text/css" rel="stylesheet">
+</head>
+</head>
+<body>
+
+    <?php
+    if(isset($_COOKIE['jinghua'])&&$_COOKIE['jinghua']=='jinghua666666'){
+        include "edit.php";
+    }else{echo '    <div  id="login">
+        <form>
+            <input type="text" id="admin" placeholder="管理员账号" required>
+            <input type="password" id="adminPassword" placeholder="密码" required>
+            <input type="button" id="button" class="btn btn-success" value="登陆">
+        </form>
+    </div>';}
+    ?>
+</body>
+<script>
+    document.getElementById('button').addEventListener('click',function () {
+        var admin=document.getElementById('admin').value;
+        var password =document.getElementById('adminPassword').value;
+        document.cookie=admin+'='+password+';';
+        window.location.reload();//重新加载此页面
+    });//监听按钮事件把cookie插入
+</script>
+</html>
