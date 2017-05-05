32d44c5a7ae19967b1982f2e43d12643ead5cef4
bobo <linbow@foxmail.com>
2017-05-05 16:28:03 +0800

    修改了数据库连接文件，便于查找错误

--- includes/cnn.php
+++ includes/cnn.php
@@ -1,5 +1,9 @@
 <?php
 $conn=mysql_connect("localhost","bbs","qwer883762");//连接数据库
-$a=mysql_select_db ("jinghua");//选择数据库
-mysql_query("SET NAMES UTF8");//设置数据库编码
+if($conn){
+    $a=mysql_select_db ("jinghua");//选择数据库
+    mysql_query("SET NAMES UTF8");//设置数据库编码
+}else{
+    echo "<script>console.log('fail to connect to mysql')</script>";//连接失败打印错误信息
+}
 ?>
